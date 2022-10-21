9363ec18f4175e964744f1e3c601b4c426f448ae
vyvuluan <vyvuluan@gmail.com>
2022-10-21 15:57:32 +0700

    login google chưa callback

--- laravel_backend/app/Http/Middleware/Cors.php
+++ laravel_backend/app/Http/Middleware/Cors.php
@@ -17,6 +17,8 @@ class Cors
     public function handle(Request $request, Closure $next)
     {
         return $next($request)
-        ->header('Access-Control-Allow-Origin', '*');
+        ->header('Access-Control-Allow-Origin' ,'*')
+        ->header('Access-Control-Allow-Headers',  'Content-Type, X-Auth-Token, Authorization, Origin')
+        ->header('Access-Control-Allow-Methods','  POST, PUT');
     }
 }
