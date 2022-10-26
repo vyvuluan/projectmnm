ae46e790c91bdec7027fe7d6f696562456a9a9bf
Trần Hoàng Long <tranhoanglong.stt.60@gmail.com>
2022-10-26 14:28:54 +0700

    comment sp

---
+++ laravel_backend/app/Models/Comment.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'product_id',
+        'customer_id',
+        'comment',
+    ];
+    public $timestamps = false;
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
