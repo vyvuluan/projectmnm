ea1583a0c42821377024cccc601a2266806bad10
manhdo1 <dodinhmanh75@gmail.com>
2022-11-08 00:29:10 +0700

    xử lý nhân viên

--- reactjs_fontend/src/components/form-admin/employees/index.js
+++ reactjs_fontend/src/components/form-admin/employees/index.js
@@ -6,35 +6,153 @@ import { AiOutlineUserDelete, AiOutlineEdit } from "react-icons/ai";
 import { RiUserAddFill } from "react-icons/ri";
 import { BiEdit } from "react-icons/bi";
 import { BsPersonPlus } from "react-icons/bs";
+import swal from "sweetalert";
 
 import axios from "axios";
+import Pagination from "../../form/pagination";
+import UpdateNV from "./updateNV";
+import CreateAccNV from "./createAccNV";
 const Employees = () => {
   const [user, setUser] = useState([]);
-  const [showPass, setShowPass] = useState(false);
+  const [username, setUsername] = useState();
+  const [valueGT, setValueGT] = useState();
+  const [valueCV, setValueCV] = useState();
+  const [page, setPage] = useState(1);
+  const [totalPage, setTotalPage] = useState();
+  const [perPage, setPerPage] = useState();
+  const [currentPage, setCurrentPage] = useState();
+  const [show, setShow] = useState(false);
+  const [showCreateAcc, setShowCreateAcc] = useState(false);
+  const [createAcc, setCreateAcc] = useState();
 
-  const handleClosePass = () => setShowPass((prev) => !prev);
+  const handleClose = () => setShow((prev) => !prev);
+  const handleCloseCreateAcc = () => setShowCreateAcc((prev) => !prev);
 
-  const handleShowChangePass = () => {
-    setShowPass(true);
+  const handleShow = (item) => {
+    setUsername(item);
+
+    setShow(true);
+  };
+  const handleShowCreate = (item) => {
+    // setUsername(item)
+    setCreateAcc(item)
+    setShowCreateAcc(true);
+  };
+  const handlePerPage = (page) => {
+    // console.log(page);
+    setPage(page);
+  };
+  const handleChangeGT = (e) => {
+    // console.log(e.target.value);
+    setValueGT(e.target.value);
+  };
+  const handleChangeCV = (e) => {
+    // console.log(e.target.value);
+    setValueCV(e.target.value);
+  };
+  const pageNumbers = [];
+
+  for (let i = 1; i <= Math.ceil(totalPage / perPage); i++) {
+    pageNumbers.push(i);
+  }
+
+  const [addNV, setAddNV] = useState({
+    ten: "",
+    sdt: "",
+    diaChi: "",
+    gioiTinh: "",
+    cv_id: "",
+  });
+  // const [showPass, setShowPass] = useState(false);
+
+  // const handleClosePass = () => setShowPass((prev) => !prev);
+
+  // const handleShowChangePass = () => {
+  //   setShowPass(true);
+  // };
+
+  const handleInput = (e) => {
+    e.persist();
+
+    setAddNV({ ...addNV, [e.target.name]: e.target.value });
   };
   useEffect(() => {
     // const controller = new AbortController();
 
     axios
-      .get("/api/admin/manageEmployee")
+      .get(`/api/admin/manageEmployee?page=${page}`)
       .then((res) => {
-        console.log(res.data.emloyee);
         setUser(res.data.emloyee.data);
+        setTotalPage(res.data.emloyee.total);
+        setPerPage(res.data.emloyee.per_page);
+        setCurrentPage(res.data.emloyee.current_page);
       })
       .catch(function (error) {
         // handle error
         console.log(error);
       });
     // return () => controller.abort();
-  }, []);
+  }, [page]);
+
+  const handleThemNV = () => {
+    const data = {
+      ten: addNV.ten,
+      sdt: addNV.sdt,
+      diaChi: addNV.diaChi,
+      gioiTinh: valueGT,
+      cv_id: valueCV,
+    };
+    axios
+      .post("/api/admin/manageEmployee", data)
+      .then((res) => {
+        console.log(res.data);
+        if (res.data.status == 200) {
+          swal("Success", res.data.message, "success");
+        }
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+        swal("Warning", "vui lòng kiểm tra lại thông tin vừa nhập ", "warning");
+      });
+  };
 
   return (
     <>
+      <B.Modal show={show} onHide={handleClose}>
+        <B.ModalHeader closeButton className="bg-secondary">
+          <B.ModalTitle>Cập nhật nhân viên</B.ModalTitle>
+        </B.ModalHeader>
+        <B.ModalBody>
+          <UpdateNV username={username} showModal={handleClose} />
+        </B.ModalBody>
+        <B.ModalFooter className="bg-secondary">
+          <B.Button
+            variant="outline-primary"
+            className="mt-2 rounded-0"
+            onClick={handleClose}
+          >
+            Hủy bỏ
+          </B.Button>
+        </B.ModalFooter>
+      </B.Modal>
+      <B.Modal show={showCreateAcc} onHide={handleCloseCreateAcc}>
+        <B.ModalHeader closeButton className="bg-secondary">
+          <B.ModalTitle>Tạo tài khoản nhân viên</B.ModalTitle>
+        </B.ModalHeader>
+        <B.ModalBody>
+          <CreateAccNV createAcc={createAcc} showModal={handleCloseCreateAcc} />
+        </B.ModalBody>
+        <B.ModalFooter className="bg-secondary">
+          <B.Button
+            variant="outline-primary"
+            className="mt-2 rounded-0"
+            onClick={handleCloseCreateAcc}
+          >
+            Hủy bỏ
+          </B.Button>
+        </B.ModalFooter>
+      </B.Modal>
       <B.Container fluid>
         <B.Row className="pe-xl-5 mb-4">
           <B.Col lg={4}>
@@ -84,105 +202,88 @@ const Employees = () => {
           </B.Col>
         </B.Row>
 
-        <B.Row className="pe-xl-5 mb-5">
-          <B.Col lg={8}>
-            <B.Form>
+        <B.Form className="mt-2">
+          <B.Row className="pe-xl-5 mb-5">
+            <B.Col lg={6}>
               <B.FormGroup>
                 <B.FormControl
                   type="text"
+                  name="ten"
                   className="rounded-0 shadow-none mb-3"
                   placeholder="Họ và tên nhân viên"
+                  onChange={handleInput}
+                  value={addNV.ten}
+                  required
                 ></B.FormControl>
               </B.FormGroup>
               <B.FormGroup>
-                <B.FormControl
-                  type="text"
-                  className="rounded-0 shadow-none mb-3"
-                  placeholder="Email"
-                ></B.FormControl>
-              </B.FormGroup>
-              <B.FormGroup>
-                <B.FormSelect className="rounded-0 shadow-none mb-3 text-muted">
+                <B.FormSelect
+                  name="gioiTinh"
+                  // value={addNV.gioiTinh}
+                  onChange={handleChangeGT}
+                  className="rounded-0 shadow-none mb-3 text-muted"
+                  required
+                >
                   <option>Giới tính</option>
-                  <option>Nam</option>
-                  <option>Nữ</option>
-                  <option>Lưỡng long nhất thể</option>
+                  <option value={1}>Nam</option>
+                  <option value={0}>Nữ</option>
                 </B.FormSelect>
               </B.FormGroup>
               <B.FormGroup>
                 <B.FormControl
-                  type="text"
+                  name="diaChi"
+                  type="textarea"
                   className="rounded-0 shadow-none mb-3"
                   placeholder="Địa chỉ"
+                  onChange={handleInput}
+                  value={addNV.diaChi}
+                  required
                 ></B.FormControl>
               </B.FormGroup>
+            </B.Col>
+            <B.Col lg={6}>
               <B.FormGroup>
                 <B.FormControl
+                  name="sdt"
                   type="text"
                   className="rounded-0 shadow-none mb-3"
                   placeholder="Số điện thoại"
+                  onChange={handleInput}
+                  value={addNV.sdt}
+                  required
                 ></B.FormControl>
               </B.FormGroup>
               <B.FormGroup>
-                <B.FormControl
-                  type="file"
-                  className="rounded-0 shadow-none mb-3"
-                ></B.FormControl>
+                <B.FormSelect
+                  name="cv_id"
+                  // value={addNV.cv_id}
+                  required
+                  onChange={handleChangeCV}
+                  className="rounded-0 shadow-none mb-3 text-muted"
+                >
+                  <option>Chức vụ</option>
+                  <option value={1}>admin</option>
+                  <option value={2}>kho</option>
+                  <option value={3}>nhân viên</option>
+                </B.FormSelect>
               </B.FormGroup>
-            </B.Form>
-          </B.Col>
-          <B.Col lg={4}>
-            <B.Button
-              variant="outline-primary"
-              className="rounded-0 py-2 mb-2 w-100"
-            >
-              <BsPersonPlusFill className="me-2" />
-              Thêm nhân viên
-            </B.Button>
-            <B.Button
-              variant="outline-primary"
-              className="rounded-0 py-2 mb-2 w-100"
-            >
-              <FaUserEdit className="me-2" />
-              Sửa nhân viên
-            </B.Button>
-            <B.Button
-              variant="outline-primary"
-              className="rounded-0 py-2 mb-1 w-100"
-            >
-              <AiOutlineUserDelete className="me-2" />
-              Xóa nhân viên
-            </B.Button>
-            <hr />
-
-            <B.FormGroup className="d-flex d-inline-block justify-content-between">
-              <B.FormControl
-                type="text"
-                className="rounded-0 shadow-none mb-1 w-75"
-                placeholder="Username"
-              ></B.FormControl>
-              <B.FormSelect className="rounded-0 shadow-none ms-2 mb-2 text-muted w-25">
-                <option>Quyền</option>
-                <option>Administrator</option>
-                <option>Thủ kho</option>
-                <option>Nhân viên</option>
-              </B.FormSelect>
-            </B.FormGroup>
-            <B.Button
-              variant="outline-primary"
-              className="rounded-0 py-2 w-100"
-            >
-              <RiUserAddFill className="me-2" />
-              Cấp tài khoản
-            </B.Button>
-          </B.Col>
-        </B.Row>
+              <B.Button
+                variant="outline-primary"
+                className="rounded-0 py-2 mb-2 w-100"
+                onClick={handleThemNV}
+              >
+                <BsPersonPlusFill className="me-2" />
+                Thêm nhân viên
+              </B.Button>
+            </B.Col>
+          </B.Row>
+        </B.Form>
 
         {/* table hien thi tai khoan */}
         <B.Row className="pe-xl-5">
           <B.Col lg className="d-grd gap-2 mx-auto table-responsive mb-5">
             <B.FormGroup className="d-flex d-inline-block justify-content-between mb-2">
-              <B.FormSelect
+              {/* <B.FormSelect
                 className="rounded-0 shadow-none"
                 style={{ width: "200px" }}
               >
@@ -190,7 +291,7 @@ const Employees = () => {
                 <option>Từ A-Z</option>
                 <option>Theo ID</option>
                 <option>Theo quyền</option>
-              </B.FormSelect>
+              </B.FormSelect> */}
             </B.FormGroup>
             <B.Table className="table-borderless border border-secondary text-center mb-0">
               <thead
@@ -198,9 +299,6 @@ const Employees = () => {
                 style={{ backgroundColor: "#edf1ff" }}
               >
                 <tr>
-                  <th>
-                    <input type="checkbox" />
-                  </th>
                   <th>ID</th>
                   <th>Họ và tên</th>
                   {/* <th>Email</th> */}
@@ -218,22 +316,27 @@ const Employees = () => {
                   // }
                   return (
                     <tr>
-                      <td className="align-middle">
-                        <input type="checkbox" />
-                      </td>
                       <td className="align-middle">{item.id}</td>
                       <td className="align-middle">{item.ten}</td>
                       {/* <td className="align-middle">{item.email}</td> */}
                       <td className="align-middle">
                         {item.gioiTinh == 1 ? "Nam" : "Nữ"}
                       </td>
-                      <td className="align-middle">{item.diaChi}</td>
+                      <td
+                        className="align-middle"
+                        style={{ wordBreak: "break-word", width: "400px" }}
+                      >
+                        {item.diaChi}
+                      </td>
                       <td className="align-middle">{item.sdt}</td>
 
                       <td className="align-middle fs-5 text-primary  ">
-                        <BiEdit />
+                        <BiEdit onClick={() => handleShow(item)} />
                         {item.user_id == null ? (
-                          <BsPersonPlus className="ms-4" />
+                          <BsPersonPlus
+                            onClick={() => handleShowCreate(item)}
+                            className="ms-4"
+                          />
                         ) : null}
                       </td>
                     </tr>
@@ -242,6 +345,11 @@ const Employees = () => {
               </tbody>
             </B.Table>
           </B.Col>
+          <Pagination
+            currentPage={currentPage}
+            totalPage={pageNumbers}
+            handlePerPage={handlePerPage}
+          />
         </B.Row>
       </B.Container>
     </>
