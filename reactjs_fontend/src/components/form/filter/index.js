354e2add14232947beaa9553c8bb1d2c71a5fd07
manhdo1 <dodinhmanh75@gmail.com>
2022-11-15 23:48:17 +0700

    filter product

--- reactjs_fontend/src/components/form/filter/index.js
+++ reactjs_fontend/src/components/form/filter/index.js
@@ -4,72 +4,37 @@ import React, { useEffect, useState } from "react";
 import Button from "react-bootstrap/Button";
 import "./style.css";
 import MinimumDistanceSlider from "./rangePrice";
-const Filter = () => {
-  const [viewNSX, setViewNSX] = useState([]);
-  const [idNSX, setIdNSX] = useState();
+const Filter = ({
+  handleFilter,
+  handleToggle,
+  value1,
+  handleChange1,
+  valuetext,
+  Checked,
+  viewNSX,
+  handleChange,
+}) => {
+  // const [viewNSX, setViewNSX] = useState([]);
+  const [idNSX, setIdNSX] = useState([]);
 
-  const [key, setKey] = useState(0);
   const [uncheckAll, setUncheckAll] = useState(true);
+
   const clearClickHandler = () => {
     setUncheckAll(true);
-    setKey((k) => k + 1);
   };
   const clearClickHandlerAll = () => setUncheckAll(false);
-  useEffect(() => {
-    axios
-      .get(`/api/nsx`)
-      .then((res) => {
-        if (res.data.status == 200) {
-          // console.log(res.data.Nsx);
-          setViewNSX(res.data.Nsx);
-          // setIdNSX(res.data.Nsx.id)
-        }
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  }, []);
-  const changeValue = (e) => {
-    console.log(e.target.value);
-  };
 
-  // rangeMoney
-  const minDistance = 10;
-  function valuetext(value) {
-    // console.log(value);
-    return value;
-  }
-  const [value1, setValue1] = React.useState([0, 50000000]);
-
-  const handleChange1 = (event, newValue, activeThumb) => {
-    if (!Array.isArray(newValue)) {
-      return;
-    }
-
-    if (activeThumb === 0) {
-      setValue1([Math.min(newValue[0], value1[1] - minDistance), value1[1]]);
-    } else {
-      setValue1([value1[0], Math.max(newValue[1], value1[0] + minDistance)]);
+  let dataIDNSX = [];
+  let idNSX1 = viewNSX.map((item) => {
+    return dataIDNSX.push(item.id);
+  });
+  const handleChangeAll = (e) => {
+    if (e.target.name === "all") {
+      setUncheckAll(true);
+      Checked.length = 0;
     }
   };
-  const data = [1, 2];
-  //  console.log(value1);
-  useEffect(() => {
-    axios
-      .get(
-        `/api/sort-chitiet-minmax?nsx_id=1,2&giaMin=${value1[0]}&giaMax=${value1[1]}`
-      )
-      .then((res) => {
-        if (res.data.status == 200) {
-          // console.log(res.data.Nsx);
-        }
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  }, []);
 
-  // console.log(viewNSX);
   var NSX_HTML = (
     <>
       <div className="border-bottom mb-4 pb-4">
@@ -81,30 +46,31 @@ const Filter = () => {
               className="custom-control-input"
               defaultChecked={uncheckAll}
               checked={uncheckAll}
-              id="price-all"
-              onClick={clearClickHandler}
-              value={0}
-              onChange={changeValue}
+              id="all"
+              name="all"
+              onClick={handleChangeAll}
+              value={idNSX1}
+              onChange={handleChange}
             ></input>
-            <label className="custom-control-label" htmlFor="price-all">
+            <label className="custom-control-label" htmlFor="all">
               Tất cả
             </label>
           </div>
-          {viewNSX.map((item) => {
+          {viewNSX.map((item, index) => {
             return (
               <>
                 <div
-                  key={key}
+                  key={item.id}
                   className="custom-control custom-checkbox d-flex align-items-center justify-content-between mb-3"
                 >
                   <input
                     type="checkbox"
-                    className="custom-control-input"
-                    defaultChecked={false}
+                    className="custom-control-input checkbox"
                     id={item.id}
                     name={item.tenNSX}
                     value={item.id}
-                    onChange={changeValue}
+                    onChange={() => handleToggle(item.id)}
+                    checked={Checked.indexOf(item.id) === -1 ? false : true}
                     onClick={clearClickHandlerAll}
                   ></input>
                   <label className="custom-control-label" htmlFor={item.id}>
@@ -134,11 +100,16 @@ const Filter = () => {
       </div>
     </>
   );
+
   return (
     <>
       {NSX_HTML}
       {Price_HTML}
-      <Button style={{ float: "right" }} variant="danger">
+      <Button
+        style={{ float: "right" }}
+        variant="danger"
+        onClick={handleFilter}
+      >
         Lọc
       </Button>
     </>
