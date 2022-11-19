bb2f59c76998fa6101c294ba918adbb6d1006b59
manhdo1 <dodinhmanh75@gmail.com>
2022-11-19 08:40:25 +0700

    fix footer

--- reactjs_fontend/src/components/form-admin/widget/index.js
+++ reactjs_fontend/src/components/form-admin/widget/index.js
@@ -219,25 +219,7 @@ const Widget = ({ dataWidget }) => {
                   <div className="h5 mb-0 pl-3 font-weight-bold text-gray-800">
                     {SLBan}
                   </div>
-                  {/* <div className="row no-gutters align-items-center">
-                    <div className="col-auto">
-                      <div className="h5 pl-4 mb-0 mr-3 font-weight-bold text-gray-800">
-                        50%
-                      </div>
-                    </div>
-                    <div className="col">
-                      <div className="progress progress-sm mr-2">
-                        <div
-                          className="progress-bar bg-info"
-                          role="progressbar"
-                          style={{ width: "50%" }}
-                          aria-valuenow="50"
-                          aria-valuemin="0"
-                          aria-valuemax="100"
-                        ></div>
-                      </div>
-                    </div>
-                  </div> */}
+                  
                 </div>
                 <div className="col-auto">
                   {/* <i className="fas fa-clipboard-list fa-2x text-gray-300"></i> */}
