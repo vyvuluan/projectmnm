b9aab5c9ef9732a7eae806c1690c09c04ca090e1
ATwinn <dinhducluong110@gmail.com>
2022-11-21 18:40:28 +0700

    sua thanh component in

--- reactjs_fontend/src/components/form-admin/donHang/index.js
+++ reactjs_fontend/src/components/form-admin/donHang/index.js
@@ -9,6 +9,8 @@ import { RiRefreshLine } from 'react-icons/ri'
 import { BsFillExclamationSquareFill } from 'react-icons/bs'
 import { useDownloadExcel } from 'react-export-table-to-excel';
 import { ReactSearchAutocomplete } from 'react-search-autocomplete'
+import Bill from '../print/bill'
+import BillKho from '../print/billkho'
 
 const checkStatus = [
   { id: 0, name: 'Chờ xác nhận' },
@@ -258,10 +260,6 @@ const DonHang = () => {
     setSearchlist([]);
   }
 
-  let date = new Date().toLocaleString("vi-VN", { day: '2-digit' });
-  let month = new Date().toLocaleString("vi-VN", { month: "long" });
-  let year = new Date().getFullYear();
-
   const SortStt = (e) => {
     var key = '';
     switch (e) {
@@ -341,198 +339,33 @@ const DonHang = () => {
 
   return (
     <>
+      {/* In hoa don va phieu xuat kho */}
       <B.Modal size='lg' show={showPrint} onHide={handleClosePrint}>
         <B.Tabs activeKey={pxtab} onSelect={(k) => setpxtab(k)}>
           <B.Tab eventKey={1} title="Hóa đơn">
             <div ref={componentRef}>
-              <B.ModalHeader>
-                <B.ModalTitle>
-                  <div className='text-primary fw-bold'>
-                    L3M
-                    <span className='text-dark'>SHOP</span>
-                    <p className='fs-6 text-dark fw-semibold'>Mã hóa đơn: {viewOrder && viewOrder.id}</p>
-                  </div>
-                </B.ModalTitle>
-                <B.ModalTitle>
-                  <div>
-                    Hóa đơn bán hàng
-                    <p className='fs-6'>Ngày {date} {month} Năm {year}</p>
-                  </div>
-                </B.ModalTitle>
-              </B.ModalHeader>
-              <B.Row className='px-3 mt-2'>
-                <div className='fs-6'>Công ty TNHH thương mại L3M</div>
-                <div className='fs-6'>Địa chỉ: 273 An D. Vương, Phường 3, Quận 5, Thành phố Hồ Chí Minh</div>
-                <div className='fst-italic fs-6'>Hotline: 0123498765<span className='ms-5'>Email: l3mstore@gmail.com</span></div>
-              </B.Row>
-              <hr />
-              <B.ModalBody>
-                <B.Row>
-                  <B.FormGroup className='d-flex justify-content-between'>
-                    <B.FormLabel className='fs-6'>Họ và tên khách hàng:</B.FormLabel>
-                    <B.FormLabel className='fs-6 ms-2 mb-3 text-success'>{viewOrder && viewOrder.tenKH}</B.FormLabel>
-                  </B.FormGroup>
-                  <B.FormGroup className='d-flex justify-content-between'>
-                    <B.FormLabel className='fs-6'>Số điện thoại khách hàng:</B.FormLabel>
-                    <B.FormLabel className='fs-6 ms-2 mb-3 text-success'>{viewOrder && viewOrder.sdt}</B.FormLabel>
-                  </B.FormGroup>
-                  <B.FormGroup className='d-flex justify-content-between'>
-                    <B.FormLabel className='fs-6'>Địa chỉ:</B.FormLabel>
-                    <B.FormLabel className='fs-6 ms-2 mb-3 text-success text-end'>{viewOrder && viewOrder.diaChi}</B.FormLabel>
-                  </B.FormGroup>
-                  <B.FormGroup className='d-flex justify-content-between'>
-                    <B.FormLabel className='fs-6'>Phương thức thanh toán:</B.FormLabel>
-                    <B.FormLabel className='fs-6 ms-2 mb-3 text-success'>{viewOrder && viewOrder.pt_ThanhToan}</B.FormLabel>
-                  </B.FormGroup>
-                </B.Row>
-                <hr />
-                <B.Row>
-                  <B.Table responsive='sm' className='table-borderless border border-muted mb-0'>
-                    <thead className='text-dark'>
-                      <tr>
-                        <th>Tên sản phẩm</th>
-                        <th>Số lượng</th>
-                        <th>Bảo hành</th>
-                        <th>Giá</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {viewOrder && viewOrder.pxct.map((prod) => {
-                        return (
-                          <>
-                            <tr key={prod.product.id}>
-                              <td>{prod.product.tenSP}</td>
-                              <td>{prod.soluong}</td>
-                              <td>{prod.product.baoHanh} tháng</td>
-                              <td>{formatMoney(prod.product.gia)}</td>
-                            </tr>
-                            <tr>
-                              <td></td>
-                            </tr>
-                          </>
-                        )
-                      })}
-                    </tbody>
-                    <tfoot className='border-top'>
-                      <tr>
-                        <td></td>
-                        <td></td>
-                        <td>Tạm tính: </td>
-                        <td>{viewOrder && viewOrder.discount !== 0 ? formatMoney(viewOrder?.tongTien / (1 - viewOrder?.discount / 100)) : formatMoney(viewOrder?.tongTien)}</td>
-                      </tr>
-                      <tr>
-                        <td></td>
-                        <td></td>
-                        <td>Giảm giá: </td>
-                        <td>{viewOrder && viewOrder.discount}%</td>
-                      </tr>
-                      <tr>
-                        <td></td>
-                        <td></td>
-                        <td className='fw-semibold'>Tổng tiền: </td>
-                        <td>{formatMoney(viewOrder && viewOrder.tongTien)}</td>
-                      </tr>
-                    </tfoot>
-                  </B.Table>
-                </B.Row>
-                <B.Row className='px-3 mt-2'>
-                  <B.Row className='mb-5'>
-                    <B.Col>
-                      <p>Khách hàng
-                        <p className='fst-italic ms-3'>Ký tên</p>
-                      </p>
-                    </B.Col>
-                    <B.Col className='text-end'>
-                      <p>Nhân viên bán hàng
-                        <p className='fst-italic me-5'>Ký tên</p></p>
-                    </B.Col>
-                  </B.Row>
-                  <div className='fst-italic mt-5'>Vui lòng giữ lại hóa đơn trong vòng 1 tháng sau khi mua hàng</div>
-                </B.Row>
-              </B.ModalBody>
+              <Bill
+                billCode={viewOrder?.id}
+                hotenKH={viewOrder?.tenKH}
+                sdt={viewOrder?.sdt}
+                diaChi={viewOrder?.diaChi}
+                pttt={viewOrder?.pt_ThanhToan}
+                data={viewOrder}
+                discount={viewOrder?.discount}
+                tongTien={viewOrder?.tongTien}
+              />
             </div>
           </B.Tab>
           <B.Tab eventKey={2} title="Phiếu xuất kho">
             <div ref={componentRefPx}>
-              <B.ModalHeader>
-                <B.ModalTitle>
-                  <div className='text-primary fw-bold'>
-                    L3M
-                    <span className='text-dark'>SHOP</span>
-                    <p className='fs-6 text-dark fw-semibold'>Mã hóa đơn: {viewOrder && viewOrder.id}</p>
-                  </div>
-                </B.ModalTitle>
-                <B.ModalTitle>
-                  <div>
-                    Phiếu yêu cầu xuất kho
-                    <p className='fs-6'>Ngày {date} {month} Năm {year}</p>
-                  </div>
-                </B.ModalTitle>
-              </B.ModalHeader>
-              <B.ModalBody>
-                <B.Row>
-                  <B.FormGroup className='d-flex justify-content-between'>
-                    <B.FormLabel className='fs-6'>Họ và tên khách hàng:</B.FormLabel>
-                    <B.FormLabel className='fs-6 ms-2 mb-3 text-success'>{viewOrder && viewOrder.tenKH}</B.FormLabel>
-                  </B.FormGroup>
-                  <B.FormGroup className='d-flex justify-content-between'>
-                    <B.FormLabel className='fs-6'>Số điện thoại khách hàng:</B.FormLabel>
-                    <B.FormLabel className='fs-6 ms-2 mb-3 text-success'>{viewOrder && viewOrder.sdt}</B.FormLabel>
-                  </B.FormGroup>
-                  <B.FormGroup className='d-flex justify-content-between'>
-                    <B.FormLabel className='fs-6'>Địa chỉ:</B.FormLabel>
-                    <B.FormLabel className='fs-6 ms-2 mb-3 text-success text-end'>{viewOrder && viewOrder.diaChi}</B.FormLabel>
-                  </B.FormGroup>
-                  <B.FormGroup className='d-flex justify-content-between'>
-                    <B.FormLabel className='fs-6'>Phương thức thanh toán:</B.FormLabel>
-                    <B.FormLabel className='fs-6 ms-2 mb-3 text-success'>{viewOrder && viewOrder.pt_ThanhToan}</B.FormLabel>
-                  </B.FormGroup>
-                </B.Row>
-                <hr />
-                <B.Row>
-                  <B.Table responsive='sm' className='table-borderless border border-muted mb-0'>
-                    <thead className='text-dark'>
-                      <tr>
-                        <th>Mã sản phẩm</th>
-                        <th>Tên sản phẩm</th>
-                        <th>Số lượng</th>
-                        <th>Giá</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {viewOrder && viewOrder.pxct.map((prod) => {
-                        return (
-                          <>
-                            <tr key={prod.product.id}>
-                              <td>{prod.product.id}</td>
-                              <td>{prod.product.tenSP}</td>
-                              <td>{prod.soluong}</td>
-                              <td>{formatMoney(prod.product.gia)}</td>
-                            </tr>
-                            <tr>
-                              <td></td>
-                            </tr>
-                          </>
-                        )
-                      })}
-                    </tbody>
-                  </B.Table>
-                </B.Row>
-                <B.Row className='px-3 mt-2'>
-                  <B.Row className='mb-5'>
-                    <B.Col>
-                      <p>Nhân viên kho
-                        <p className='fst-italic ms-4'>Ký tên</p>
-                      </p>
-                    </B.Col>
-                    <B.Col className='text-end'>
-                      <p>Nhân viên bán hàng
-                        <p className='fst-italic me-5'>Ký tên</p></p>
-                    </B.Col>
-                  </B.Row>
-                  <div className='fst-italic mt-5'>Nhân viên kho giữ lại phiếu sau khi xuất kho</div>
-                </B.Row>
-              </B.ModalBody>
+              <BillKho
+                billCode={viewOrder?.id}
+                tenKH={viewOrder?.tenKH}
+                sdt={viewOrder?.sdt}
+                diaChi={viewOrder?.diaChi}
+                pttt={viewOrder?.pt_ThanhToan}
+                data={viewOrder}
+              />
             </div>
           </B.Tab>
         </B.Tabs>
