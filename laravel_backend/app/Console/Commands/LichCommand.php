9e0d0256619c0bd064b2d718e1d1ad6b426e6646
vyvuluan <vyvuluan@gmail.com>
2022-11-24 17:45:14 +0700

    lap lich

---
+++ laravel_backend/app/Console/Commands/LichCommand.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\ConfirmMail;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+
+class LichCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:lich';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $today = Carbon::now();
+        $confirms = ConfirmMail::get();
+        foreach ($confirms as $confirm) {
+            if (strtotime($today) > strtotime(strtotime($confirm->create_at . ' + 3 minute'))) {
+                $confirm->delete();
+                User::where('email', $confirm->email)->delete();
+            }
+        }
+    }
+}
