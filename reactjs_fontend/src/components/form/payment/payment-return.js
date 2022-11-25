7010ac0fca7bc707fe898f18d741f0e901726741
ATwinn <dinhducluong110@gmail.com>
2022-11-25 23:10:28 +0700

    Update payment-return.js

--- reactjs_fontend/src/components/form/payment/payment-return.js
+++ reactjs_fontend/src/components/form/payment/payment-return.js
@@ -1,12 +1,11 @@
 import React, { useEffect, useState, useRef } from 'react'
 import * as Bt from 'react-bootstrap'
 import { BsFillCheckCircleFill } from 'react-icons/bs'
-import { Link, useNavigate } from 'react-router-dom'
+import { Link, useNavigate, useSearchParams } from 'react-router-dom'
 import Breadcum from '../breadcum/index'
 
 function useInterval(callback, delay) {
     const savedCallback = useRef();
-
     // Remember the latest callback.
     useEffect(() => {
         savedCallback.current = callback;
@@ -24,6 +23,16 @@ function useInterval(callback, delay) {
 export default function Paymentreturn() {
     const [countDown, setCountDown] = useState(10);
     const navigate = useNavigate();
+    const [searchParam, useSearchParam] = useSearchParams();
+    const [orderStatus, setStatus] = useState();
+
+    useEffect(() => {
+        if (searchParam.get("status") === "success") {
+            setStatus(true);
+        } else if (searchParam.get("status") === "fail") {
+            setStatus(false);
+        }
+    }, [searchParam])
 
     useInterval(() => {
         if (countDown > 0) {
@@ -41,11 +50,22 @@ export default function Paymentreturn() {
 
             <Bt.Container fluid pt={5} className='mb-5'>
                 <Bt.Row className='px-xl-5 py-5 text-center'>
-                    <h1>Cảm ơn bạn đã mua hàng</h1>
-                    <div className='d-flex justify-content-center'>
-                        <p className='me-3'>Trở về trang chủ trong <Link to='/' className='text-decoration-none'>{countDown}</Link></p>
-                        <span>Kiểm tra đơn hàng <Link to='/myorder' className='text-decoration-none'>tại đây</Link></span>
-                    </div>
+                    {orderStatus === true ?
+                        <>
+                            <h1>Cảm ơn bạn đã mua hàng</h1>
+                            <div className='d-flex justify-content-center'>
+                                <p className='me-3'>Trở về trang chủ trong <Link to='/' className='text-decoration-none'>{countDown}</Link></p>
+                                <span>Kiểm tra đơn hàng <Link to='/myorder' className='text-decoration-none'>tại đây</Link></span>
+                            </div>
+                        </>
+                        :
+                        <>
+                            <h1>Thanh toán thất bại</h1>
+                            <div className='d-flex justify-content-center'>
+                                <p className='me-3'>Trở về trang chủ trong <Link to='/' className='text-decoration-none'>{countDown}</Link></p>
+                                <span>Kiểm tra đơn hàng <Link to='/myorder' className='text-decoration-none'>tại đây</Link></span>
+                            </div>
+                        </>}
                 </Bt.Row>
             </Bt.Container>
         </>
